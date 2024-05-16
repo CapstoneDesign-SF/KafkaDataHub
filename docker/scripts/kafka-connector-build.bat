2d5291f4a31c7d0638884a0c5affcf2fcc6a7c51
박민제 <phone7101@naver.com>
2024-05-16 13:12:41 +0900

    [추가] mqtt connector 추가

--- docker/scripts/kafka-connector-build.bat
+++ docker/scripts/kafka-connector-build.bat
@@ -1,2 +1,2 @@
 cd ../plugins/
-docker build -t "pmj6823/kafka-connect-with-connector-jdbc:0.1" -f "../plugins/kafka-connect-jdbc" .
+docker build -t "pmj6823/kafka-connect-with-connector-jdbc:0.1" -f "../plugins/kafka-connect-with-plugins" .
